756751418988c7908c103dcc8543416678c4c5fa
함정훈 <hamjunghun2@gmail.com>
2024-03-12 20:29:41 +0900

    v0.3 week02_prime_number

--- week02_prime_number.py
+++ week02_prime_number.py
@@ -1,11 +1,11 @@
 number = int(input("number: "))
 
 cnt = 0
-for i in range(1, number+1):
+for i in range(2, number):
     if number % i == 0:
         cnt += 1
 
-if cnt == 2:
+if cnt == 0:
     print(f"{number} is prime number")
 else:
     print(f"{number} is not prime number")
