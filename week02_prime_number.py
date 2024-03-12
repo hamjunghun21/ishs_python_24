04f8d4b069510165b69f6e6e45431f51d032c9e3
함정훈 <hamjunghun2@gmail.com>
2024-03-12 21:24:41 +0900

    v0.7 week02_prime_number

--- week02_prime_number.py
+++ week02_prime_number.py
@@ -4,11 +4,13 @@ is_prime_number = True # Change variable name from cnt to is_prime_number for re
 if number < 2:
     is_prime_number = False
 else:
-    for i in range(2, number):
+    i = 2
+    while i < number: # Change for loop to while loop
         if number % i == 0:
             is_prime_number = False # Calculate with boolean operator instead of addition operation
             break # The loop ends when the first divisor is found.
-
+        print(i, end = " ")
+        i += 1 # Increment i by 1
 if is_prime_number: # Remove comparison operators
     print(f"{number} is prime number")
 else:
