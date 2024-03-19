a2c1586f5a93434fb6aeebd14924f989b4e41e17
함정훈 <hamjunghun2@gmail.com>
2024-03-19 20:55:56 +0900

    v1.5 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,24 +1,22 @@
-def is_prime_number(n) -> bool:
+def is_prime_number(k) -> bool:
     """
     Prime number decision function
-    :param n: positive integer
-    :return: True if n is prime number, False otherwise
+    :param k: positive integer
+    :return: True if k is prime number, False otherwise
     """
-    pass
+    if k < 2:
+        return False
+    else:
+        i = 2
+        while i*i <= k: # reduce loop operation
+            if k % i == 0:
+                return False # Calculate with boolean operator instead of addition operation
+            i += 1 # Increment i by 1
+        return True
 
 
 start, end = list(map(int, input("start number and end number: ").split()))
 
-for k in range(start, end+1):
-    is_prime_number = True # Change variable name from cnt to is_prime_number for readability.
-    if k < 2:
-        is_prime_number = False
-    else:
-        i = 2
-        while i*i <= k:
-            if k % i == 0:
-                is_prime_number = False # Calculate with boolean operator instead of addition operation
-                break # The loop ends when the first divisor is found.
-            #print(i, end = " ")
-            i += 1 # Increment i by 1
-        if is_prime_number: print(k, end = " ")
+for k in range(start, end + 1):
+    if is_prime_number(k):
+        print(k, end = " ")
