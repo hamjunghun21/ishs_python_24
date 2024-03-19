630e407ab09733ee895c77937b78c89ae6a1f4e5
함정훈 <hamjunghun2@gmail.com>
2024-03-19 20:28:49 +0900

    v1.4 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,3 +1,12 @@
+def is_prime_number(n) -> bool:
+    """
+    Prime number decision function
+    :param n: positive integer
+    :return: True if n is prime number, False otherwise
+    """
+    pass
+
+
 start, end = list(map(int, input("start number and end number: ").split()))
 
 for k in range(start, end+1):
