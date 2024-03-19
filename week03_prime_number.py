7cee476b92c36b2764a21d542394c2154d771e87
함정훈 <hamjunghun2@gmail.com>
2024-03-19 19:20:39 +0900

    v10 week03_prime_number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,17 +1,16 @@
-number = int(input("number: "))
+start = int(input("start number: "))
+end = int(input("end number: "))
 
-is_prime_number = True # Change variable name from cnt to is_prime_number for readability.
-if number < 2:
-    is_prime_number = False
-else:
-    i = 2
-    while i*i <= number: # bug fix
-        if number % i == 0:
-            is_prime_number = False # Calculate with boolean operator instead of addition operation
-            break # The loop ends when the first divisor is found.
-        #print(i, end = " ")
-        i += 1 # Increment i by 1
-if is_prime_number: # Remove comparison operators
-    print(f"{number} is prime number")
-else:
-    print(f"{number} is not prime number")
+for k in range(start, end+1):
+    is_prime_number = True # Change variable name from cnt to is_prime_number for readability.
+    if k < 2:
+        is_prime_number = False
+    else:
+        i = 2
+        while i*i <= k:
+            if k % i == 0:
+                is_prime_number = False # Calculate with boolean operator instead of addition operation
+                break # The loop ends when the first divisor is found.
+            #print(i, end = " ")
+            i += 1 # Increment i by 1
+        if is_prime_number: print(k, end = " ")
