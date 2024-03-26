9989668d5bfbdf3ffa5d033ba9ecfd62479c3295
함정훈 <hamjunghun2@gmail.com>
2024-03-26 19:15:43 +0900

    v0.2 power

--- week04_power.py
+++ week04_power.py
@@ -1,3 +1,16 @@
+def power(b, e) -> int:
+    """
+    Power function
+    :param b: base number
+    :param e: exponent number
+    :return: power result value
+    """
+    result = 1
+    for i in range(e):
+        result = result * b
+    return result
+
 base, exponent = map(int,input("i:").split())
 print(f"{base}^{exponent} = {base**exponent}")
 print(f"{base}^{exponent} = {pow(base, exponent)}") #built in fuction
+print(f"{base}^{exponent} = {power(base, exponent)}") #custom fuction
