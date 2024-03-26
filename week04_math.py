c45ec877059ef24e1a4c2ea2bf4d581deeb42341
함정훈 <hamjunghun2@gmail.com>
2024-03-26 21:25:21 +0900

    v0.2 math

--- week04_math.py
+++ week04_math.py
@@ -1,47 +1,20 @@
-def is_prime_number(n) -> bool:
-    """
-    Prime number decision function
-    :param n: positive integer
-    :return: True if k is prime number, False otherwise
-    """
-    if n < 2:
-        return False
-    else:
-        i = 2
-        while i*i <= n: # reduce loop operation
-            if n % i == 0:
-                return False # Calculate with boolean operator instead of addition operation
-            i += 1 # Increment i by 1
-        return True
+def print_fx(fx):
+    texts = "f(x) = "
+    expo = len(fx) - 1
 
-def power(b, e) -> int:
-    """
-    Power function
-    :param b: base number
-    :param e: exponent number
-    :return: power result value
-    """
-    result = 1
-    for _ in range(e):
-        result = result * b
-    return result
+    for i in range(len(fx)):
+        coef = fx[i]
+        if coef == 0:
+            continue
+            expo = expo - 1
 
-while True:
-    menu = input("1) prime number 2) power 3) divmod 4) quit :") #ValueError: invalid literal for int() with base 10: '에헤'
-    if menu == "1":
-        start, end = list(map(int, input("start number and end number: ").split()))
-        for k in range(start, end + 1):
-            if is_prime_number(k):
-                print(k, end=" ")
-    elif menu == "2":
-        base, exponent = map(int, input("i:").split())
-        print(f"{base}^{exponent} = {power(base, exponent)}")
-    elif menu == "3":
-        dividend, divisor = map(int, input("Input dividend & divisor number: ").split())
-        print(f"{dividend} // {divisor} = {divmod(dividend, divisor)[0]}")
-        print(f"{dividend} % {divisor} = {divmod(dividend, divisor)[1]}")
-    elif menu == "4":
-        print("exit progress")
-        exit() #or break()
-    else:
-        print("Please choose from the menu")
+        if coef >= 0 and i != 0:
+            texts = texts + "+"
+
+        texts = texts + str(coef) +"x^" + str(expo) + " "
+        expo = expo - 1
+    return texts
+
+coefficient = [5, -2, 0, 6]
+
+print(print_fx(coefficient))
