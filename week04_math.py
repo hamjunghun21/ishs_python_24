89d5b423d06dbefe88f1cfab07c435ca625f5416
함정훈 <hamjunghun2@gmail.com>
2024-03-26 21:40:05 +0900

    v0.3 math

--- week04_math.py
+++ week04_math.py
@@ -4,17 +4,39 @@ def print_fx(fx):
 
     for i in range(len(fx)):
         coef = fx[i]
-        if coef == 0:
-            continue
-            expo = expo - 1
 
-        if coef >= 0 and i != 0:
+        if coef == 0:
+            expo = expo -1
+            continue
+
+        if coef > 0 and i != 0:
             texts = texts + "+"
 
         texts = texts + str(coef) +"x^" + str(expo) + " "
         expo = expo - 1
     return texts
 
+def input_fx(fx, x):
+    sum_ = 0
+    expo = len(fx) - 1
+    for i in range(len(fx)):
+        sum_ += fx[i] * (x**expo)
+        expo -= 1
+    return sum_
+
+def calculat_fx(fx, x):
+    expo = len(fx) - 1
+    result = 0
+
+    for k in range(len(fx)):
+        coef = fx[k]
+        result = result + coef * x ** expo
+        expo = expo - 1
+    return result
+
 coefficient = [5, -2, 0, 6]
 
 print(print_fx(coefficient))
+x = int(input("Input x value : "))
+print(input_fx(coefficient, x))
+print(calculat_fx(coefficient,x))
