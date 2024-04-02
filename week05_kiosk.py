41aebba04d69d11e250edfbbe77cab75438788f0
함정훈 <hamjunghun2@gmail.com>
2024-04-02 20:40:16 +0900

    v0.3 kiosk

--- week05_kiosk.py
+++ week05_kiosk.py
@@ -1,21 +1,31 @@
 # ISHS CAFFE
 # americano 1500,latte 2500
-beverage = ["Americano", "Latte"]
-prices = ["1500", "2500"]
+beverage = ["Americano", "Latte", "Iced tea"]
+prices = [1500, 2500, 2300]
+quantity = [0, 0, 0]
 total_price = 0
 
 while True:
-    menu = input("1) Americano 2) Latte 3) End order : ")
-    if menu == "3":
+    menu = input("1) Americano 2) Latte 3) Iced tea 4) End order : ")
+    if menu == "4":
         print("Your order had been accepted.")
         break
     elif menu == "1":
         print("You ordered Americano coffee. The price is 1500 won.")
-        total_price = total_price + int(prices[0])
+        total_price = total_price + prices[0]
+        quantity[0] = quantity[0] + 1
     elif menu == "2":
         print("You ordered a cafe latte. The price is 2500 won.")
-        total_price = total_price + int(prices[1])
+        total_price = total_price + prices[1]
+        quantity[1] = quantity[1] + 1
+    elif menu == "3":
+        print("You ordered a iced tea. The price is 2300 won.")
+        total_price = total_price + prices[2]
+        quantity[2] = quantity[2] + 1
     else:
         print(f"The menu number {menu} you ordered does not exit. Please choose from the menu.")
 
+print(f"{beverage[0]} x{quantity[0]}")
+print(f"{beverage[1]} x{quantity[1]}")
+print(f"{beverage[2]} x{quantity[2]}")
 print(f"The total amount is {total_price} won.")
