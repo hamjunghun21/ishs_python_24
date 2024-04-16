f0936b350041ba6e821b0feeccd2929cbe82a9cb
함정훈 <hamjunghun2@gmail.com>
2024-04-16 19:07:23 +0900

    v0.9 kiosk

--- week05_kiosk.py
+++ week05_kiosk.py
@@ -1,6 +1,6 @@
 # ISHS CAFFE
 # americano 1500,latte 2500
-beverage = ["Americano", "Latte", "Iced tea"]
+beverage = ["americano coffee", "latte", "iced tea"]
 prices = [1500, 2500, 2300]
 quantity = [0, 0, 0]
 total_price = 0
@@ -17,15 +17,15 @@ while True:
         print("Your order had been accepted.")
         break
     elif menu == "1":
-        print("You ordered Americano coffee. The price is 1500 won.")
+        print(f"You ordered {beverage[0]}. The price is {prices[0]} won.")
         total_price = total_price + prices[0]
         quantity[0] = quantity[0] + 1
     elif menu == "2":
-        print("You ordered a cafe latte. The price is 2500 won.")
+        print(f"You ordered {beverage[1]}. The price is {prices[1]} won.")
         total_price = total_price + prices[1]
         quantity[1] = quantity[1] + 1
     elif menu == "3":
-        print("You ordered a iced tea. The price is 2300 won.")
+        print(f"You ordered {beverage[2]}. The price is {prices[2]} won.")
         total_price = total_price + prices[2]
         quantity[2] = quantity[2] + 1
     else:
