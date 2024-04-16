149b6db51db4ee1041d79611c0ca7f49849fe385
함정훈 <hamjunghun2@gmail.com>
2024-04-16 19:34:22 +0900

    v0.5 math

--- week04_math.py
+++ week04_math.py
@@ -12,7 +12,8 @@ def print_fx(fx):
         if coef > 0 and i != 0:
             texts = texts + "+"
 
-        texts = texts + str(coef) +"x^" + str(expo) + " "
+        #texts = texts + str(coef) +"x^" + str(expo) + " "
+        texts = texts + f"{coef}x^{expo}"
         expo = expo - 1
     return texts
 
