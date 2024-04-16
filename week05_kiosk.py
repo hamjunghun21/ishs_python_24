5130ac2cac0aaee88bd96e674a12110c28bf5069
함정훈 <hamjunghun2@gmail.com>
2024-04-16 19:16:38 +0900

    v1.0 kiosk

--- week05_kiosk.py
+++ week05_kiosk.py
@@ -1,5 +1,16 @@
 # ISHS CAFFE
 # americano 1500,latte 2500
+def select_menu(index):
+    """
+    display menu, calculate, total price and count quantity
+    :param index: index of list
+    :return: None
+    """
+    global total_price
+    print(f"You ordered {beverage[index]}. The price is {prices[index]} won.")
+    total_price = total_price + prices[index]
+    quantity[index] = quantity[index] + 1
+
 beverage = ["americano coffee", "latte", "iced tea"]
 prices = [1500, 2500, 2300]
 quantity = [0, 0, 0]
@@ -17,17 +28,11 @@ while True:
         print("Your order had been accepted.")
         break
     elif menu == "1":
-        print(f"You ordered {beverage[0]}. The price is {prices[0]} won.")
-        total_price = total_price + prices[0]
-        quantity[0] = quantity[0] + 1
+        select_menu(0)
     elif menu == "2":
-        print(f"You ordered {beverage[1]}. The price is {prices[1]} won.")
-        total_price = total_price + prices[1]
-        quantity[1] = quantity[1] + 1
+        select_menu(1)
     elif menu == "3":
-        print(f"You ordered {beverage[2]}. The price is {prices[2]} won.")
-        total_price = total_price + prices[2]
-        quantity[2] = quantity[2] + 1
+        select_menu(2)
     else:
         print(f"The menu number {menu} you ordered does not exit. Please choose from the menu.")
 
